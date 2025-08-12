215d3478e0da30c1dd6c795ff56c3cb59a983481
Tanishq Jain <tanishqjain1109@gmail.com>
2025-08-12 19:14:58 +0530

    checking

--- backend/dist/keys.js
+++ backend/dist/keys.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.company_password = exports.company_mail = exports.JWT_SECRET = exports.mongo_db = exports.google_api_key = exports.port_number = void 0;
-exports.port_number = process.env.PORT || 3000;
+exports.port_number = 3000;
 exports.google_api_key = "AIzaSyDnbDahWYbNPHZH-Okhsjm6ldjuLqlUFnE";
 exports.mongo_db = "mongodb+srv://Drill_Endeavour:Tanishq%4022@hero.agqxn.mongodb.net/";
 exports.JWT_SECRET = "hello";
